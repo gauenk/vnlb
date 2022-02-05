a8d5d4c488942bbf18c1fb5681dcb3f08bb078ae
gauenk <kent.gauen@gmail.com>
2022-02-05 12:20:38 -0500

    add nvn exp pck

---
+++ lib/vnlb/nvn/__init__.py
@@ -0,0 +1,30 @@
+"""
+This package contains scripts for experiments
+exploring the characteristics of
+(i) patch content and (ii) their realized, noisy versions
+on the quality of our NNNL method.
+"""
+
+
+def load_clean():
+    pass
+
+def load_noisy():
+    pass
+
+def load_nl():
+    pass
+
+def load_nn():
+    pass
+
+def nn_v_nl():
+
+    # -- load output images --
+    clean = load_clean()
+    noisy = load_noisy()
+    deno_nl = load_nl()
+    deno_nn = load_nn()
+
+    return deno_nl,deno_nn
+
